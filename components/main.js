f5c77f9986745d81fb76ae6ada7bc35bb848cfb7
Thao Dau <96876233+ThaoDau@users.noreply.github.com>
2023-03-22 21:42:15 +0700

    update

--- components/main.js
+++ components/main.js
@@ -7,7 +7,7 @@ const main = () => {
             <h3>ABOUT ME</h3>
             <h2 class="mb-3">THÔNG TIN CÁ NHÂN</h2>
             <p class="w-75">Em là Đậu Phương Thảo, sinh viên kì 5. Với sự tò mò và không ngừng học hỏi, em luôn dành cho mình thời gian để tìm hiểu các công nghệ mới hoặc học ux/ui</p>
-            <button class="view-detail rounded">XEM CV</button>
+            <button class="view-detail rounded"><a href="https://drive.google.com/file/d/1fI7aOQ34HL0MbT5SCgVLeR9PWrH7Ghua/view?usp=sharing">XEM CV</a></button>
         </div>
         <div class="box-right ">
             <div class="intro  ">
@@ -17,9 +17,9 @@ const main = () => {
             <div class="status-study ">
   
                 <br>
-                <p>Học trường: cao đẳng fpt polytechnic</p>
-                <p>Chuyên ngành: thiết kế web </p>
-                <p>Kì học: 5</p>
+                <p>Học trường: Cao đẳng FPT Polytechnic Hà Nội</p>
+                <p>Chuyên ngành: Thiết kế web  </p>
+                <p></p>
                 <p>Project em thường làm gần đây: 
                 <ul>
                     <li>Vakia_project</li>
@@ -27,7 +27,7 @@ const main = () => {
                     <li>ACADU_project</li>
                 </ul>
                 </p>
-                <p>Mục tiêu tương lai: đi làm và tiếp thu nhiều kiến thức ngôn ngữ lập trình.  </p>
+                <p>Mục tiêu tương lai: Đi làm học hỏi kinh nghiệm và tiếp thu nhiều kiến thức từ các tiền bối đi trước. </p>
                 </p>
             </div>
         </div>
